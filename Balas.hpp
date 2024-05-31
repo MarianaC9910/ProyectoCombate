c126adb1c3d2868304dbb40248f8f48126b13bcf
MarianaC9910 <158246568+MarianaC9910@users.noreply.github.com>
2024-05-31 07:40:21 -0700

    Update Balas.hpp

--- Balas.hpp
+++ Balas.hpp
@@ -1,1 +1,27 @@
+#ifndef Balas_hpp
+#define Balas_hpp
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <string>
+#include <cmath>
 
+using namespace std;
+using namespace sf;
+
+Class Bala{
+  private:
+    int posx,posy;
+    
+
+    public: 
+
+    Sprite bullet;
+
+    Bala();
+    Bala(float startX, float startY);
+      
+    void draw(RenderWindow& window);
+    void mover(RenderWindow& window, Keyboard::Key key);
+    void aparecer(int x, int y);
+    void setSize(float width, float height);
+}
