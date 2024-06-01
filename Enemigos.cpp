508ad974251136d54215227ab56d0290cace1344
MarianaC9910 <158246568+MarianaC9910@users.noreply.github.com>
2024-05-31 22:45:26 -0700

    Update Enemigos.cpp

--- Enemigos.cpp
+++ Enemigos.cpp
@@ -1,71 +1,67 @@
 #include "Enemigos.hpp"
 // #include "rlutil.h"
 
-    Enemigo::Enemigo(){
-        // enemigo.setTexture(this->mounstruo);
-    }
-
-    Enemigo::Enemigo(int vid){
+Enemigo::Enemigo(){
+    this->vida = rand()%20+71;
+    this->danio = rand()%10+51; 
+    this->posx=700;
+    this->posy=rand()%801;
+    enemigo.setPosition(posx, posy);
+}
+//vida, danio y posicion  aleatorios
+Enemigo::Enemigo(int vid){
         this->vida = vid;
+        this->danio = 10; 
+        this->posx = 700;
+        this->posy = rand()%801;
+        enemigo.setPosition(posx, posy);
         // enemigo.setTexture(mounstruo);
-    }
-    Enemigo::Enemigo(int vid, int dan){
+}
+Enemigo::Enemigo(int vid, int dan){
         this->vida = vid;
-        this->danio = dan;
+        this->danio = dan; 
+        this->posx = 700;
+        this->posy = rand()%801;
+        enemigo.setPosition(posx, posy);
         // enemigo.setTexture(mounstruo);
-    }
-    void Enemigo::cargarTexturas(){
-        // if (!mounstruo.loadFromFile("texturas/tex.jpg"))
-    {
-        cout << "Error al cargar la textura" << endl;
-    }
+}
+void Enemigo::draw(RenderWindow& window){
+        window.draw(enemigo);
+}
+void Enemigo::mover(int posActY){
+        Vector2u ventanaSize = window.getSize(); 
+        int speed=rand()%12;
+        
+        int windowWidth = ventanaSize.x;  // obtenemos el ancho de la ventana
+        int windowHeight = ventanaSize.y; // obtenemos el alto de la ventana
 
-    // if (!textura_derecha.loadFromFile("texturas/der.jpg"))
-    // {
-    //     cout << "Error al cargar la textura" << endl;
-    // }
-    
-    // if (!textura_izquierda.loadFromFile("texturas/izq.jpg"))
-    // {
-    //     cout << "Error al cargar la textura" << endl;
-    // }
-    
-    // if (!textura_arriba.loadFromFile("texturas/frente.jpg"))
-    // {
-    //     cout << "Error al cargar la textura" << endl;
-    // }
-    
-    // if (!textura_arriba_d.loadFromFile("texturas/diagonal_ad.jpg"))
-    // {
-    //     cout << "Error al cargar la textura" << endl;;
-    // }
-    
-    // if (!textura_arriba_i.loadFromFile("texturas/diagonal_ai.jpg"))
-    // {
-    //     cout << "Error al cargar la textura" << endl;
-    // }
-    
-    // if (!textura_abajo.loadFromFile("texturas/atras.jpg"))
-    // {
-    //     cout << "Error al cargar la textura" << endl;
-    // }
-    
-    // if (!textura_abajo_d.loadFromFile("texturas/diagonal_atd.jpg"))
-    // {
-    //     cout << "Error al cargar la textura" << endl;
-    // }
-    
-    // if (!textura_abajo_i.loadFromFile("texturas/diagonal_ati.jpg"))
-    // {
-    //     cout << "Error al cargar la textura" << endl;
-    // }
+        if(posActY<this->posy){
+            if(this->posy-speed<=0&&this->posy>1&&this->posy-speed<posActY){
+                this->posy--;
+            }else{
+                this->posy-=speed;
+            }
+        }else if(posActY>this->posy){
+            if(this->posy+speed>=800&&this->posy<799&&this->posy+speed>posActY){
+                this->posy++;
+            }else{
+                this->posy-=speed;
+            }
+        }else{
+            atacar();
+        }
+
+    enemigo.setPosition(posx, posy);
+}
+    //revisa coordenadas del jugador y se mueve hacia ellas 
+    void Enemigo::aparecer(int x, int y){
+        posx = x;
+        posy = y;
+        enemigo.setPosition(posx, posy);
     }
-    void draw(RenderWindow& window){
+    void Enemigo::atacar(){
 
     }
-    void mover(){}
-    //revisa coordenadas del jugador y se mueve hacia ellas
-    void aparecer(int x, int y){}
-    void atacar(){}
-    void apuntar(RenderWindow& window){}
-    void setSize(float width, float height){}
+    void Enemigo::setSize(float width, float height){
+        enemigo.setScale(width/enemigo.getLocalBounds().width, height/enemigo.getLocalBounds().height);
+    }
