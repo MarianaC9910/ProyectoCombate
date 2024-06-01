523aa035782f147902a7840f47206ce2b8277a1d
MarianaC9910 <158246568+MarianaC9910@users.noreply.github.com>
2024-05-31 22:42:46 -0700

    Update Enemigos.hpp

--- Enemigos.hpp
+++ Enemigos.hpp
@@ -4,28 +4,29 @@
 #include <SFML/Graphics.hpp>
 #include <string>
 #include <cmath>
+#include <vector>
 using namespace std;
 using namespace sf;
+
+class Bala;
+
 class Enemigo{
 private:
     int vida;
     int danio;
     int posx,posy;
+    vector<Bala>disparos;
 public: 
-    Texture mounstruo; 
-
     Sprite enemigo;
 
     Enemigo();
     Enemigo(int vid);
     Enemigo(int vid, int dan);
 
-     void cargarTexturas();
     void draw(RenderWindow& window);
-    void mover();//revisa coordenadas del jugador y se mueve hacia ellas 
+    void mover(int posActY);//revisa coordenadas del jugador y se mueve hacia ellas 
     void aparecer(int x, int y);
     void atacar();
-    void apuntar(RenderWindow& window);
     void setSize(float width, float height);
 };
 
