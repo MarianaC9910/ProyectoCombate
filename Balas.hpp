7277e6e079f44cb78479f2c3369f596b58b9a8cd
MarianaC9910 <158246568+MarianaC9910@users.noreply.github.com>
2024-06-01 20:04:47 -0700

    Update Balas.hpp

--- Balas.hpp
+++ Balas.hpp
@@ -8,20 +8,23 @@
 using namespace std;
 using namespace sf;
 
-Class Bala{
+class Bala{
   private:
     int posx,posy;
-    
+    int speed;
+    bool dir;//0 para Jugador, 1 para enemigos
+    bool estado;//estado==true cuando la bala no ha salido del mapa o intersectado con un personaje
 
     public: 
 
     Sprite bullet;
 
-    Bala();
-    Bala(float startX, float startY);
+    Bala(float startX, float startY, bool direc);
       
     void draw(RenderWindow& window);
     void mover(RenderWindow& window, Keyboard::Key key);
     void aparecer(int x, int y);
     void setSize(float width, float height);
-}
+};
+
+#endif
