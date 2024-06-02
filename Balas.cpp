719300e28cdb81004a67f41d64d66861b3e45479
MarianaC9910 <158246568+MarianaC9910@users.noreply.github.com>
2024-06-01 20:05:18 -0700

    Update Balas.cpp

--- Balas.cpp
+++ Balas.cpp
@@ -1,1 +1,31 @@
+#include "Balas.hpp"
+using namespace std;
+Bala::Bala(float startX, float startY, bool direc){
+    this->posy=startX;
+    this->posx=startY;
+    this->dir=direc;
+    this->speed=rand()%10+20;
+    bullet.setPosition(posx, posy);
+};
+      
+void Bala::draw(RenderWindow& window){
+    if(this->estado==true){
+        window.draw(bullet);
+    }
+}
+void Bala::mover(RenderWindow& window, Keyboard::Key key){
+    if(this->dir==0){
+        this->posx+=speed;
+    }else{
+        this->posx-=speed;
+    }
+}
+void Bala::aparecer(int x, int y){
+    posx = x;
+    posy = y;
+    bullet.setPosition(posx, posy);
+}
+void Bala::setSize(float width, float height){
+    bullet.setScale(width/bullet.getLocalBounds().width, height/bullet.getLocalBounds().height);
+}
 
