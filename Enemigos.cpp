89a5e0068e579e817421dff5196b76f701891ef3
MarianaC9910 <158246568+MarianaC9910@users.noreply.github.com>
2024-06-01 20:03:47 -0700

    Update Enemigos.cpp

--- Enemigos.cpp
+++ Enemigos.cpp
@@ -1,71 +1,81 @@
 #include "Enemigos.hpp"
-// #include "rlutil.h"
+#include <vector>
+Enemigo::Enemigo()
+{
+    this->vida = 20;
+    this->danio = 10; 
+    this->posx=100;
+    this->posy=100;
+    mounstruo.setPosition(posx, posy);
+};
 
-    Enemigo::Enemigo(){
-        // enemigo.setTexture(this->mounstruo);
+Enemigo::Enemigo(float startX, float startY)
+{
+    this->vida = 20;
+    this->danio = 10; 
+    posx = startX;
+    posy = startY;
+    mounstruo.setPosition(posx, posy);
+    
+}
+Enemigo::Enemigo(int vid,float startX, float startY)
+{
+    this->vida = vid;
+    this->danio=10;
+    posx = startX;
+    posy = startY;
+    mounstruo.setPosition(posx, posy);
+    mounstruo.setPosition(posx, posy);
+}
+Enemigo::Enemigo(int vid, int dan, float startX, float startY)
+{
+    this->vida = vid;
+    this->danio = dan;
+    posx = startX;
+    posy = startY;
+    mounstruo.setPosition(posx, posy);
+}
+
+void Enemigo::draw(RenderWindow& window){
+    window.draw(mounstruo);
+}
+
+
+void Enemigo::mover(RenderWindow& window, int posActY){
+    Vector2u ventanaSize = window.getSize(); 
+    int speed=rand()%12;
+    
+    int windowWidth = ventanaSize.x;  // obtenemos el ancho de la ventana
+    int windowHeight = ventanaSize.y; // obtenemos el alto de la ventana
+
+    if(posActY<this->posy){
+        if(this->posy-speed<=0&&this->posy>1&&this->posy-speed<posActY){
+            this->posy--;
+        }else{
+            this->posy-=speed;
+        }
+    }else if(posActY>this->posy){
+        if(this->posy+speed>=800&&this->posy<799&&this->posy+speed>posActY){
+            this->posy++;
+        }else{
+            this->posy-=speed;
+        }
+    }else{
+        atacar();
     }
 
-    Enemigo::Enemigo(int vid){
-        this->vida = vid;
-        // enemigo.setTexture(mounstruo);
-    }
-    Enemigo::Enemigo(int vid, int dan){
-        this->vida = vid;
-        this->danio = dan;
-        // enemigo.setTexture(mounstruo);
-    }
-    void Enemigo::cargarTexturas(){
-        // if (!mounstruo.loadFromFile("texturas/tex.jpg"))
-    {
-        cout << "Error al cargar la textura" << endl;
-    }
+    mounstruo.setPosition(posx, posy);
+}
+    //revisa coordenadas del jugador y se mueve hacia ellas 
+void Enemigo::aparecer(int x, int y){
+    posx = x;
+    posy = y;
+    mounstruo.setPosition(posx, posy);
+}
 
-    // if (!textura_derecha.loadFromFile("texturas/der.jpg"))
-    // {
-    //     cout << "Error al cargar la textura" << endl;
-    // }
-    
-    // if (!textura_izquierda.loadFromFile("texturas/izq.jpg"))
-    // {
-    //     cout << "Error al cargar la textura" << endl;
-    // }
-    
-    // if (!textura_arriba.loadFromFile("texturas/frente.jpg"))
-    // {
-    //     cout << "Error al cargar la textura" << endl;
-    // }
-    
-    // if (!textura_arriba_d.loadFromFile("texturas/diagonal_ad.jpg"))
-    // {
-    //     cout << "Error al cargar la textura" << endl;;
-    // }
-    
-    // if (!textura_arriba_i.loadFromFile("texturas/diagonal_ai.jpg"))
-    // {
-    //     cout << "Error al cargar la textura" << endl;
-    // }
-    
-    // if (!textura_abajo.loadFromFile("texturas/atras.jpg"))
-    // {
-    //     cout << "Error al cargar la textura" << endl;
-    // }
-    
-    // if (!textura_abajo_d.loadFromFile("texturas/diagonal_atd.jpg"))
-    // {
-    //     cout << "Error al cargar la textura" << endl;
-    // }
-    
-    // if (!textura_abajo_i.loadFromFile("texturas/diagonal_ati.jpg"))
-    // {
-    //     cout << "Error al cargar la textura" << endl;
-    // }
-    }
-    void draw(RenderWindow& window){
-
-    }
-    void mover(){}
-    //revisa coordenadas del jugador y se mueve hacia ellas
-    void aparecer(int x, int y){}
-    void atacar(){}
-    void apuntar(RenderWindow& window){}
-    void setSize(float width, float height){}
+void Enemigo::atacar(){
+   //disparos.push_back(posx);     
+}
+void Enemigo::setSize(float width, float height){
+    mounstruo.setScale(width/mounstruo.getLocalBounds().width, height/mounstruo.getLocalBounds().height);
+}
