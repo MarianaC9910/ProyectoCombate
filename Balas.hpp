45b0e64deca50142f4bfe669888f2a3f96aa8b54
MarianaC9910 <158246568+MarianaC9910@users.noreply.github.com>
2024-06-02 22:04:59 -0700

    Update Balas.hpp

--- Balas.hpp
+++ Balas.hpp
@@ -4,24 +4,31 @@
 #include <SFML/Graphics.hpp>
 #include <string>
 #include <cmath>
+//#include "Globales.hpp"
+#include "Enemigos.hpp"
 
 using namespace std;
 using namespace sf;
 
-Class Bala{
+
+
+class Bala{
   private:
-    int posx,posy;
-    
+    int speed;
+    bool estado;//estado==true cuando la bala no ha salido del mapa o intersectado con un personaje
 
     public: 
+    int posx,posy;
+    int dir;//1: abajo, 2:izquierda, 3:arriba, 4:derecha
 
     Sprite bullet;
 
-    Bala();
-    Bala(float startX, float startY);
+    Bala(float startX, float startY, int direc);
       
     void draw(RenderWindow& window);
-    void mover(RenderWindow& window, Keyboard::Key key);
+    void mover(RenderWindow& window);
     void aparecer(int x, int y);
     void setSize(float width, float height);
-}
+};
+
+#endif
