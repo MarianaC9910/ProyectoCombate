ac108b936216da5561bbfad4845d4aa06cedfee9
MarianaC9910 <158246568+MarianaC9910@users.noreply.github.com>
2024-06-02 22:04:33 -0700

    Update Enemigos.cpp

--- Enemigos.cpp
+++ Enemigos.cpp
@@ -1,8 +1,10 @@
 #include "Enemigos.hpp"
 #include <vector>
+//#include "Globales.hpp"
+#include "Balas.hpp"
 Enemigo::Enemigo()
 {
-    this->vida = 20;
+    this->vida = 10;
     this->danio = 10; 
     this->posx=100;
     this->posy=100;
@@ -14,6 +16,7 @@ Enemigo::Enemigo(float startX, float startY, int type)
     this->vida = 20;
     this->danio = 10; 
     this->tipo = type;
+    this->time=0;
     posx = startX;
     posy = startY;
     mounstruo.setPosition(posx, posy);
@@ -45,6 +48,7 @@ void Enemigo::draw(RenderWindow& window){
 
 
 void Enemigo::mover(RenderWindow& window, Jugador player){
+    
     Vector2u ventanaSize = window.getSize(); 
     
     int windowWidth = ventanaSize.x;  // obtenemos el ancho de la ventana
@@ -55,7 +59,8 @@ void Enemigo::mover(RenderWindow& window, Jugador player){
         }else if(this->posx>player.posx){
             this->posx-=2;
         }else{
-            atacar();
+            if(this->time%100==0)
+                atacar();
         }
     }else{
         if(this->posy<player.posy){
@@ -63,9 +68,11 @@ void Enemigo::mover(RenderWindow& window, Jugador player){
         }else if(this->posy>player.posy){
             this->posy-=2;
         }else{
-            atacar();
+            if(this->time%100==0)
+                atacar();        
         }
     }
+    this->time++;
     mounstruo.setPosition(posx, posy);
 }
     //revisa coordenadas del jugador y se mueve hacia ellas 
@@ -76,8 +83,44 @@ void Enemigo::aparecer(int x, int y){
 }
 
 void Enemigo::atacar(){
-   //disparos.push_back(posx);     
+    Texture bulletv;
+    Texture bulleth;
+
+    bulletv.loadFromFile("texturas/bullet_v.png");
+    bulleth.loadFromFile("texturas/bullet_h.png");
+
+    float startXB, startYB;
+    Bala disparo(50, 50, this->tipo);
+    switch(disparo.dir){
+        case 1://la bala va hacia abajo
+            disparo.bullet.setTexture(bulletv);
+            startXB = this->posx+50.0f;
+            startYB = this->posy+80.0f;
+        break;
+        case 2://izquierda
+            disparo.bullet.setTexture(bulleth);
+            startXB = this->posx+20.0f;
+            startYB = this->posy+50.0f;
+        break;
+        case 3://arriba
+            disparo.bullet.setTexture(bulletv);
+            startXB = this->posx+45.0f;
+            startYB = this->posy+5.0f;
+        break;
+        case 4://derecha
+            disparo.bullet.setTexture(bulleth);
+            startXB = this->posx+80.0f;
+            startYB = this->posy+45.0f;
+        break;
+        default:
+        break;
+    }
+
+        disparo.setSize(10,10);
+        disparo.aparecer(startXB,startYB);
+        this->disparos.push_back(disparo);
 }
+
 void Enemigo::setSize(float width, float height){
     mounstruo.setScale(width/mounstruo.getLocalBounds().width, height/mounstruo.getLocalBounds().height);
 }
